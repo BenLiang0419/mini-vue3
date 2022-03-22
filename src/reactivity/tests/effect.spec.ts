d3a29e4c02e3ff8e74a89d7a1caddd0b67cee79a
liangjs <asd153200>
2022-03-22 17:16:37 +0800

    feat: add babel.config.js

---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,14 @@
+
+describe("effect", () => {
+    it('bar', function () {
+        const user = reactive({
+            age: 10
+        });
+
+        let nextAge;
+        effect(() => {
+
+        })
+
+    });
+})
